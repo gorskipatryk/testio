6935c9bd7faa6e92ed41248ad9bcb1e891b0b46c
Patryk Górski <patryk.gorski2@dxc.com>
2023-02-13 17:48:45 +0100

    Introduce auth module

---
+++ testio/testioAuth/Supporting files/testioAuth.h
@@ -0,0 +1,18 @@
+//
+//  testioAuth.h
+//  testioAuth
+//
+//  Created by Patryk Górski on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testioAuth.
+FOUNDATION_EXPORT double testioAuthVersionNumber;
+
+//! Project version string for testioAuth.
+FOUNDATION_EXPORT const unsigned char testioAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testioAuth/PublicHeader.h>
+
+
