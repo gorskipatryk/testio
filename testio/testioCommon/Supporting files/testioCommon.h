9ccbf0df4faa942ab9dfc640cacfaf13df5c43e4
Patryk Górski <patryk.gorski2@dxc.com>
2023-02-13 17:43:53 +0100

    Introduce common module

---
+++ testio/testioCommon/Supporting files/testioCommon.h
@@ -0,0 +1,18 @@
+//
+//  testioCommon.h
+//  testioCommon
+//
+//  Created by Patryk Górski on 13/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testioCommon.
+FOUNDATION_EXPORT double testioCommonVersionNumber;
+
+//! Project version string for testioCommon.
+FOUNDATION_EXPORT const unsigned char testioCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testioCommon/PublicHeader.h>
+
+
