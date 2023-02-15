ca4c9e567c36063747d8af8f123e1a365752a0be
Patryk Górski <patryk.gorski2@dxc.com>
2023-02-15 17:35:19 +0100

    Introduce Server Browser module

---
+++ testio/testioServerBrowser/SupportingFiles/testioServerBrowser.h
@@ -0,0 +1,18 @@
+//
+//  testioServerBrowser.h
+//  testioServerBrowser
+//
+//  Created by Patryk Górski on 15/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testioServerBrowser.
+FOUNDATION_EXPORT double testioServerBrowserVersionNumber;
+
+//! Project version string for testioServerBrowser.
+FOUNDATION_EXPORT const unsigned char testioServerBrowserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testioServerBrowser/PublicHeader.h>
+
+
